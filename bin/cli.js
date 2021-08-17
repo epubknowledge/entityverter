1fee20446ec1ccbc0859735077fa291f9ae2d0a6
codingChewie <codingChewie@gmail.com>
2021-08-17 09:55:01 -0500

    added CLI

--- bin/cli.js
+++ bin/cli.js
@@ -13,11 +13,12 @@ const cli = () => {
     .version(version)
     .name(name)
     .description(description)
-    .option('-i, --input [folder]', 'Directory to be piped in', process.cwd())
-    .option('-o, --output [folder]', 'Directory file should be written to', process.cwd())
+    .option('-i, --input [file]', 'File to read in to be converted', false)
+    .option('-r, --result [boolean]', 'Directory to be piped in', false)
+    .option('-o, --output [folder]', 'Directory file should be written to', false)
     .action(async () => {
-      const { input, output } = program._optionValues
-      await main(input, output)
+      const { input, result, output, cli = true } = program._optionValues
+      await main(input, result, output, cli)
     })
 
   program.parse(process.argv)
