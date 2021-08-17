97f97828565b871a1c00e03f4b9a89c08acc86bc
codingChewie <codingChewie@gmail.com>
2021-08-17 10:13:31 -0500

    build object

--- common/objectSetup.js
+++ common/objectSetup.js
@@ -1,10 +1,26 @@
-const { absolute } = require('./paths')
+const { absolute, parentDir } = require('./paths')
 const { create } = require('./tmp')
 
-module.exports = (i, o) => {
+module.exports = (i, r, o, cli) => {
+  const input = absolute(i) // Builds an absolute path for input
+  const output = o !== false ? absolute(i) : input // Copies the input path if it doesn't exist or builds an absolute path
+
+  // Return results
+  let results
+  if (r === true) {
+    results = {
+      status: true,
+      path: parentDir(output),
+    }
+  } else {
+    results = false
+  }
+
   return {
-    input: absolute(i), // Builds an absolute path for input
-    output: o !== false ? absolute(i) : false, // Copies the input path if it doesn't exist or builds an absolute path
-    tmp: create(), // Building locally pass in: true
+    input,
+    output,
+    results,
+    tmp: create(true), // Building locally? Pass in: true
+    cli,
   }
 }
