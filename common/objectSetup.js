4d0acc9454c787c8b0e99d1b98fd96ccca7a5eb2
codingChewie <codingChewie@gmail.com>
2021-08-19 16:10:59 -0500

    corrected CLI output

--- common/objectSetup.js
+++ common/objectSetup.js
@@ -6,7 +6,7 @@ module.exports = (i, r, o, cli) => {
   const results = r === true ? true : false // Return results
   if (cli === true) {
     const input = absolute(i) // Builds an absolute path for input
-    const output = o !== false ? absolute(i) : input // Copies the input path if it doesn't exist or builds an absolute path
+    const output = o !== false ? absolute(o) : input // Copies the input path if it doesn't exist or builds an absolute path
     const file = fileObj(input) // File details
     return {
       input,
