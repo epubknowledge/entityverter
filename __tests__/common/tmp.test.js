af79e9333da98382bf8c66e3d2564d833bbcf3dc
codingChewie <codingChewie@gmail.com>
2021-08-19 08:34:40 -0500

    added tmp tests

--- __tests__/common/tmp.test.js
+++ __tests__/common/tmp.test.js
@@ -12,3 +12,15 @@ test('Remove files in tmp', async () => {
   await remove(tmpObj)
   expect(ex(tmpObj)).toEqual(false)
 })
+
+const devTmp = create(true)
+
+test('Create developer tmp', async () => {
+  await expect(devTmp).toHaveProperty('dir')
+  await expect(devTmp).toHaveProperty('nuke')
+})
+
+test('Remove developer tmp', async () => {
+  await remove(devTmp)
+  expect(ex(devTmp)).toEqual(false)
+})
