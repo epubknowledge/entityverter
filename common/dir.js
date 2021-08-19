d852485564045d02b2e08d079f6019fe7795a24f
codingChewie <codingChewie@gmail.com>
2021-08-19 07:57:10 -0500

    removed unzipper

--- common/dir.js
+++ common/dir.js
@@ -1,5 +1,4 @@
 const fs = require('fs-extra')
-const unzipper = require('unzipper')
 const chalk = require('chalk')
 
 module.exports = {
@@ -13,24 +12,4 @@ module.exports = {
     }
   },
   dirEmpty: dir => fs.emptyDirSync(dir), // Empties a directory
-  unzip: obj => {
-    // Unzips an ePub to a directory
-    return new Promise((res, rej) => {
-      try {
-        const s = fs
-          .createReadStream(`${obj.tmp.dir}/${obj.epub.filename}`)
-          .pipe(unzipper.Extract({ path: `${obj.tmp.dir}/${obj.epub.dir}` }))
-        s.on('close', () => {
-          res(fs.removeSync(`${obj.tmp.dir}/${obj.epub.filename}`))
-        })
-      } catch (e) {
-        console.log(chalk.red('Error:'), chalk.white(e.message))
-        rej(process.exit(1))
-      }
-    })
-  },
-  zip: z => {
-    // Zips a directory to an ePub
-    console.log('zip called')
-  },
 }
