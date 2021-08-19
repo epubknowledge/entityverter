17d6b3a3c88f81343f73f05622e8fe1964b35fbe
codingChewie <codingChewie@gmail.com>
2021-08-19 08:24:22 -0500

    built tests

--- __tests__/common/paths.test.js
+++ __tests__/common/paths.test.js
@@ -1,13 +1,19 @@
 path = require('path')
 const { homedir } = require('os')
 
-const { desktop, doc, absolute } = require('./../../common/paths')
+const { desktop, doc, absolute, parentDir } = require('./../../common/paths')
 
+// Desktop
 test('Build desktop path', () => expect(desktop()).toBe(path.join(homedir(), 'Desktop')))
 
+// Doc
 test('Build document path', () => expect(doc()).toBe(path.join(homedir(), 'Documents')))
 
-test('Build absolute path', () => expect(absolute('foo')).toBe(`${process.cwd()}/foo`))
+// Absolute
+test('Build absolute path', () => {
+  expect(absolute('foo')).toBe(path.join(process.cwd(), 'foo'))
+  expect(absolute(process.cwd())).toBe(path.join(process.cwd()))
+})
 
 test('absolute path error', () => {
   try {
@@ -17,3 +23,9 @@ test('absolute path error', () => {
     mock.mockRestore()
   } catch {}
 })
+
+// parentDir
+test('Test parentDir', () =>
+  expect(parentDir(path.join(process.cwd(), '__tests__', 'files', 'bad.txt'))).toBe(
+    path.join(process.cwd(), '__tests__', 'files'),
+  ))
