42a0080438549feb7b9b59c8cbac3ecbed74ebb5
codingChewie <codingChewie@gmail.com>
2021-08-19 16:10:28 -0500

    modified for CLI

--- common/objectSetup.js
+++ common/objectSetup.js
@@ -1,19 +1,26 @@
-const { fileExt, fileObj } = require('./file')
+const { fileObj } = require('./file')
 const { absolute } = require('./paths')
 const { create } = require('./tmp')
 
 module.exports = (i, r, o, cli) => {
-  const input = absolute(i) // Builds an absolute path for input
-  const output = o !== false ? absolute(i) : input // Copies the input path if it doesn't exist or builds an absolute path
-  const file = fileObj(input) // File details
   const results = r === true ? true : false // Return results
-
-  return {
-    input,
-    output,
-    file,
-    results,
-    tmp: create(), // Building locally? Pass in: true
-    cli,
+  if (cli === true) {
+    const input = absolute(i) // Builds an absolute path for input
+    const output = o !== false ? absolute(i) : input // Copies the input path if it doesn't exist or builds an absolute path
+    const file = fileObj(input) // File details
+    return {
+      input,
+      output,
+      file,
+      results,
+      tmp: create(), // Building locally? Pass in: true
+      cli,
+    }
+  } else {
+    return {
+      data: i,
+      cli,
+      results,
+    }
   }
 }
